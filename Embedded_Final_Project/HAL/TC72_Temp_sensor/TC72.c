d7c8bb9ce343038d4c9b1f963d548cee871c310a
abdulla167 <abdullaelsayed167@yahoo.com>
2021-06-02 20:09:44 +0200

    add some sensor methods

--- Embedded_Final_Project/HAL/TC72_Temp_sensor/TC72.c
+++ Embedded_Final_Project/HAL/TC72_Temp_sensor/TC72.c
@@ -1,9 +1,18 @@
 //
 // Created by abdulla167 on ٢‏/٦‏/٢٠٢١.
 //
-#include "SPI.h"
 #include "TC72.h"
 
-void test(void){
-    SPI_initMaster();
+void init(Mode operationMode){
+    SPI_initMaster(MSB);
+    SPI_sendByte(0x08);
+    SPI_sendByte(operationMode);
+}
+
+uint8 ReadTemperature(void){
+    int8 Msb;
+    int8 Lsb;
+    SPI_sendByte(0x02);
+    Msb = SPI_recieveByte();
+    Lsb = SPI_recieveByte();
 }
--- Embedded_Final_Project/HAL/TC72_Temp_sensor/TC72.h
+++ Embedded_Final_Project/HAL/TC72_Temp_sensor/TC72.h
@@ -4,5 +4,15 @@
 
 #ifndef FINAL_PROJECT_TC72_H
 #define FINAL_PROJECT_TC72_H
-void test(void);
+#include "SPI.h"
+
+typedef uint8 Mode;
+
+#define CONTINUOUS_MODE ((uint8)0x00)
+#define ONE_SHOT_MODE ((uint8)0x05)
+#define SHUTDOWN_MODE ((uint8)0x01)
+
+void init(Mode operationMode);
+uint8 ReadTemperature(void);
+
 #endif //FINAL_PROJECT_TC72_H
