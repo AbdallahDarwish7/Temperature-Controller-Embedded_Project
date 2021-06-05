d499d41ca04c8d5b3044f08f381000a910fefd89
abdallah <abdullah.drwesh98@eng-st.cu.edu.eg>
2021-06-05 15:29:47 +0200

    solve link keypad lcd error

--- Embedded_Final_Project/main.c
+++ Embedded_Final_Project/main.c
@@ -7,12 +7,15 @@
 
 #include "Timer.h"
 #include "DIO.h"
+#include "KeyPad.h"
 
 int main(void) {
-    float Percentage = 20;
-    DIO_ChannelDir(1, 3, 0xff);
-    Timer_Init(0, TIMER_PWD_PHASE_CORRECT_MODE, TIMER_NON_INVERTED_COM);
-    PWM_SetDutyCycle(0, Percentage, TIMER_PWD_PHASE_CORRECT_MODE, TIMER_NON_INVERTED_COM);
-    Timer_Start(0, TIMER0_PRESCALER_8);
+    test_KeyPad_Lcd();
+//    /* test pwd */
+//    float Percentage = 20;
+//    DIO_ChannelDir(1, 3, 0xff);
+//    Timer_Init(0, TIMER_PWD_PHASE_CORRECT_MODE, TIMER_NON_INVERTED_COM);
+//    PWM_SetDutyCycle(0, Percentage, TIMER_PWD_PHASE_CORRECT_MODE, TIMER_NON_INVERTED_COM);
+//    Timer_Start(0, TIMER0_PRESCALER_8);
     while (1) {}
 }
