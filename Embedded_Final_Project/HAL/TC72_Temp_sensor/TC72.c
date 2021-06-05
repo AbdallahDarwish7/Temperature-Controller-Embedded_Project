3cdca5c310969c5e2fc2e40bfd6338f1baeaf09a
abdallah <abdullah.drwesh98@eng-st.cu.edu.eg>
2021-06-05 15:13:07 +0200

    add Timer

--- Embedded_Final_Project/HAL/TC72_Temp_sensor/TC72.c
+++ Embedded_Final_Project/HAL/TC72_Temp_sensor/TC72.c
@@ -1,9 +1,3 @@
 //
 // Created by abdulla167 on ٢‏/٦‏/٢٠٢١.
 //
-#include "SPI.h"
-#include "TC72.h"
-
-void test(void){
-    SPI_initMaster();
-}
--- Embedded_Final_Project/main.c
+++ Embedded_Final_Project/main.c
@@ -3,19 +3,17 @@
  *
  * Created: 6/2/2021 3:01:44 PM
  * Author : AbdallahDrwesh
- */ 
+ */
 
-#include <avr/io.h>
-
+#include "Timer.h"
 #include "DIO.h"
-#include "TC72.h"
 
-int main(void)
-{
-    /* Replace with your application code */
-	test();
-    while (1) 
-    {
-    }
+int main(void) {
+    float Percentage = 20;
+    DIO_ChannelDir(1, 3, 0xff);
+    Timer_Init(0, TIMER_PWD_PHASE_CORRECT_MODE, TIMER_NON_INVERTED_COM);
+    PWM_SetDutyCycle(0, Percentage, TIMER_PWD_PHASE_CORRECT_MODE, TIMER_NON_INVERTED_COM);
+    Timer_Start(0, TIMER0_PRESCALER_8);
+    while (1) {}
 }
 
